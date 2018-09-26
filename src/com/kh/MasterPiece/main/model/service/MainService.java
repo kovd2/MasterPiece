313199eaa95fef73580cc6d317169e32a7ff3e26
yup <yup@192.168.1.61>
2018-09-26 21:01:52 +0900

    검색(검색 후 페이지 이동, 페이징처리) 메인 상품 css

--- src/com/kh/MasterPiece/main/model/service/MainService.java
+++ src/com/kh/MasterPiece/main/model/service/MainService.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.kh.MasterPiece.board.model.vo.Attachment;
+import com.kh.MasterPiece.board.model.vo.Board;
 import com.kh.MasterPiece.main.model.dao.MainDao;
 import com.kh.MasterPiece.main.model.vo.MainTest;
+import com.kh.MasterPiece.product.model.dao.ProductDao;
 import com.kh.MasterPiece.product.model.vo.Product;
 
 import static com.kh.MasterPiece.common.JDBCTemplate.*;
@@ -14,19 +16,7 @@ import static com.kh.MasterPiece.common.JDBCTemplate.*;
 
 public class MainService {
 	
-	//메인 상품 DB 조회 메소드
-	public ArrayList<MainTest> selectProduct() {
-		Connection con = getConnection();
-		
-		ArrayList<MainTest> list = new MainDao().selectProduct(con);
-		
-				
-		close(con);
-		
-		return list;
-	}
-	
-	//GRAPHIC
+	//그래픽 페이징 처리 메소드
 	public int getlistCount1() {
 		Connection con = getConnection();
 		
@@ -36,7 +26,7 @@ public class MainService {
 		
 		return listCount;
 	}
-	//CPU
+	//CPU 페이징 처리 메소드
 	public int getlistCount2() {
 		Connection con = getConnection();
 		
@@ -46,7 +36,8 @@ public class MainService {
 		
 		return listCount;
 	}
-
+	
+	//그래픽 리스트 불러오는 메소드
 	public ArrayList<Product> graphicList(int currentPage, int limit) {
 		Connection con = getConnection();
 		
@@ -56,7 +47,8 @@ public class MainService {
 		
 		return list;
 	}
-
+	
+	//CPU 리스트 불러오는 메소드
 	public ArrayList<Product> cpuList(int currentPage, int limit) {
 		Connection con = getConnection();
 		
@@ -67,6 +59,7 @@ public class MainService {
 		return list;
 	}
 	
+	//모든 상품 불러오는 메소드
 	public ArrayList<Product> allList() {
 		Connection con = getConnection();
 		
@@ -76,7 +69,8 @@ public class MainService {
 		
 		return list;
 	}
-
+	
+	//이미지 불러오는 메소드
 	public HashMap<String, Attachment> selectImageList() {
 		Connection con = getConnection();
 		
@@ -86,20 +80,39 @@ public class MainService {
 		
 		return list;
 	}
-
-
-	public ArrayList<HashMap<String, Object>> selectImgList() {
+	
+	//메인 배너
+	public ArrayList<Board> mainBanner() {
 		Connection con = getConnection();
 		
-		ArrayList<HashMap<String, Object>> list = new MainDao().selectImgList(con);
+		ArrayList<Board> banner = new MainDao().mainBanner(con);
+		
+		close(con);
+		
+		return banner;
+	}
+	
+	//검색한 상품 리스트
+	public ArrayList<Product> searchList(String search, int currentPage, int limit) {
+		Connection con = getConnection();
+		
+		ArrayList<Product> list = new MainDao().searchList(con, search, currentPage, limit);
 		
 		close(con);
 		
 		return list;
 	}
-
-
 	
+	//검색 페이징
+	public int searchCount(String search) {
+		Connection con = getConnection();
+		
+		int listCount = new MainDao().searchCount(con, search);
+		
+		close(con);
+		
+		return listCount;
+	}
 }
 
 
